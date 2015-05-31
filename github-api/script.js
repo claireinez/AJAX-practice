0d0f12bb113cc9d1b63ee04640b0f86366555934
Claire Mitchell <claireinezmitchell@gmail.com>
2015-05-31 14:20:00 +0100

    Search GH usernames and get real name back

--- github-api/script.js
+++ github-api/script.js
@@ -1,26 +1,31 @@
-function fetchJSONFile(url, returnData) {
-        var request = new XMLHttpRequest();
+function nameUpdate () {
+    var user = document.getElementById("input").value;
+    fetchJSONFile('https://api.github.com/users/' + user, returnData);
+}
 
-        request.onreadystatechange = function () {
-                if (request.readyState === 4) {
-                        if (request.status === 200) {
-                                // console.log(request.responseText);
-                                var data = JSON.parse(request.responseText);
-                                if (typeof returnData === 'function') {
-                                        returnData(data);
-                                }
-                        }
+
+function fetchJSONFile(url, returnData) {
+    var request = new XMLHttpRequest();
+
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
+            if (request.status === 200) {
+                var data = JSON.parse(request.responseText);
+                if (typeof returnData === 'function') {
+                    returnData(data);
                 }
+            }
         }
-        request.open('GET', 'https://api.github.com/users/nofootnotes', true);
-        request.send();
+    };
+    console.log(url);
+
+    request.open('GET', url, true);
+    request.send();
 }
 
 
 function returnData (jsonData) {
-        console.log(jsonData.name);
-        var name = jsonData.name;
-        document.getElementById('name').innerHTML = name;
+    console.log(jsonData.name);
+    var name = jsonData.name;
+    document.getElementById('name').innerHTML = name;
 }
-
-fetchJSONFile('https://api.github.com/users/nofootnotes', returnData);
